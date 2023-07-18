a0b5b32cbef28812c257d9d17d0e9088e2b8e689
mindytyj <derpbritz@gmail.com>
2023-07-19 01:57:32 +0800

    Create Skeleton of Promotions Component for Home Page

---
+++ src/pages/HomePage/Promotions.jsx
@@ -0,0 +1,38 @@
+export default function Promotions() {
+  return (
+    <div className="uk-container uk-align-center">
+      <div className="uk-h3">Fade</div>
+
+      <div
+        className="uk-position-relative uk-visible-toggle uk-light"
+        tabindex="-1"
+        uk-slideshow="animation: fade"
+      >
+        <ul className="uk-slideshow-items">
+          <li>
+            <img src="" alt="" uk-cover />
+          </li>
+          <li>
+            <img src="" alt="" uk-cover />
+          </li>
+          <li>
+            <img src="" alt="" uk-cover />
+          </li>
+        </ul>
+
+        <a
+          className="uk-position-center-left uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-previous
+          uk-slideshow-item="previous"
+        ></a>
+        <a
+          className="uk-position-center-right uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-next
+          uk-slideshow-item="next"
+        ></a>
+      </div>
+    </div>
+  );
+}

d068800c816dd394d0006da80f52123721b4da65
mindytyj <derpbritz@gmail.com>
2023-07-20 01:57:26 +0800

    Amend Books Page Styling

--- src/pages/BooksPage/BooksPage.jsx
+++ src/pages/BooksPage/BooksPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import sendRequest from "../../utilities/send-request";
+import { Link } from "react-router-dom";
 
 export default function BooksPage() {
   const [books, setBooks] = useState([]);
@@ -19,17 +20,21 @@ export default function BooksPage() {
   return (
     <div className="uk-container uk-padding">
       <h1 className="uk-text-center">Books</h1>
-      <div class="uk-child-width-1-4@m" uk-grid>
+      <div className="uk-grid-column-medium uk-child-width-1-4@s uk-grid-match uk-text-center uk-grid">
         {books.map((book) => {
           return (
             <div>
               <div className="uk-card uk-card-secondary">
                 <div className="uk-card-media-top">
-                  <img src="" width="200" height="200" alt="" />
+                  <Link to={`/books/${book.id}`}>
+                    <img src="" width="300" height="300" alt="" />
+                  </Link>
                 </div>
                 <div className="uk-card-body">
-                  <h3 className="uk-card-title uk-text-center">{book.title}</h3>
-                  <p className="uk-text-center">${book.price}</p>
+                  <p>${book.price}</p>
+                  <button className="uk-button uk-button-primary uk-align-center">
+                    Add to Cart
+                  </button>
                 </div>
               </div>
             </div>

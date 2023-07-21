db559ee69a71ba97cb36a54ec2123c1b551aef9e
mindytyj <derpbritz@gmail.com>
2023-07-21 10:23:19 +0800

    Utilise Add to Cart Button and Stock Label in Main Book Details

--- src/pages/BookDetailsPage/MainBookDetails.jsx
+++ src/pages/BookDetailsPage/MainBookDetails.jsx
@@ -1,6 +1,8 @@
 import { useAtomValue } from "jotai";
 import WishlistButton from "./WishlistButton";
 import { bookAtom } from "./bookContext";
+import AddToCartButton from "../../components/AddToCartButton/AddToCartButton";
+import StockLabel from "../../components/StockLabel/StockLabel";
 
 export default function MainBookDetails() {
   const book = useAtomValue(bookAtom);
@@ -8,22 +10,26 @@ export default function MainBookDetails() {
   return (
     <div className="uk-section uk-section-secondary uk-preserve-color">
       <div className="uk-container uk-margin-left uk-margin-right">
-        <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin uk-grid">
-          <div class="uk-card-media-left uk-cover-container">
-            <img src="images/light.jpg" alt="" uk-cover />
+        <div
+          className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin"
+          data-uk-grid
+        >
+          <div className="uk-card-media-left uk-cover-container">
+            <img src="images/light.jpg" alt="" data-uk-cover />
             <canvas width="600" height="400"></canvas>
           </div>
           <div>
-            <div class="uk-card-body">
-              <h3 class="uk-card-title">{book.title}</h3>
+            <div className="uk-card-body">
+              <h3 className="uk-card-title">{book.title}</h3>
               <p>
                 by {book.first_name} {book.last_name}
               </p>
               <p>${book.price}</p>
-              <button className="uk-button uk-button-primary uk-margin-right">
-                Add to Cart
-              </button>
+              <AddToCartButton book={book} />
               <WishlistButton />
+              <p>
+                <StockLabel book={book} />
+              </p>
             </div>
           </div>
         </div>

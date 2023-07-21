4f5b4db942af250899ab3f500acec97fab6a9beb
mindytyj <derpbritz@gmail.com>
2023-07-21 16:51:21 +0800

    Remove Modal from Add to Cart Button

--- src/components/AddToCartButton/AddToCartButton.jsx
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -1,16 +1,13 @@
 import { useAtomValue } from "jotai";
-import { useState } from "react";
 import { userAtom } from "../../utilities/userContext";
 import sendRequest from "../../utilities/send-request";
 
 export default function AddToCartButton({ book }) {
   const disable = parseInt(book.qty) === 0;
   const user = useAtomValue(userAtom);
-  const [cartMsg, setCartMsg] = useState("");
 
   async function addToCart() {
     if (user === null) {
-      setCartMsg("Join as a T-WB member to add to cart.");
       return;
     }
 
@@ -20,34 +17,18 @@ export default function AddToCartButton({ book }) {
         qty: 1,
         userId: parseInt(user.id),
       });
-      setCartMsg("Book has been successfully added to your cart.");
-    } catch {
-      setCartMsg("The book is already in your cart. Consider checking out!");
+    } catch (error) {
+      console.log(error.message);
     }
   }
 
-  console.log(cartMsg);
-
   return (
-    <>
-      <button
-        className="uk-button uk-button-primary"
-        disabled={disable}
-        onClick={addToCart}
-        data-uk-toggle="target: #addToCartModal"
-      >
-        Add to Cart
-      </button>
-      <div id="addToCartModal" className="uk-flex-top" data-uk-modal>
-        <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
-          <button
-            className="uk-modal-close-default"
-            type="button"
-            data-uk-close
-          />
-          <p className="uk-text-center">{cartMsg}</p>
-        </div>
-      </div>
-    </>
+    <button
+      className="uk-button uk-button-primary"
+      disabled={disable}
+      onClick={addToCart}
+    >
+      Add to Cart
+    </button>
   );
 }

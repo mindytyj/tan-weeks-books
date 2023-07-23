a6354b5df1f1887d1c1b90cecb057479a54012f9
mindytyj <derpbritz@gmail.com>
2023-07-24 02:48:28 +0800

    Create Wishlist Item Component

---
+++ src/pages/WishlistPage/WishlistItem.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import RemoveWishlistItem from "./RemoveWishlistItem";
+
+export default function WishlistItem({ book, setWishlist, wishlist }) {
+  return (
+    <tr>
+      <td>
+        <img
+          className="uk-preserve-width uk-border-circle"
+          src="images/avatar.jpg"
+          width="40"
+          height="40"
+          alt=""
+        />
+      </td>
+      <td className="uk-text-nowrap">
+        <Link to={`/books/${book.id}`} className="uk-button-link">
+          {book.title}
+        </Link>
+      </td>
+      <td>
+        <RemoveWishlistItem
+          book={book}
+          setWishlist={setWishlist}
+          wishlist={wishlist}
+        />
+      </td>
+    </tr>
+  );
+}

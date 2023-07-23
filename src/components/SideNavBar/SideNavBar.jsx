5f7cf2acd3da4ab8cf0405608d161c71bacb7b42
mindytyj <derpbritz@gmail.com>
2023-07-24 03:23:17 +0800

    Create Side Nav Bar Component

---
+++ src/components/SideNavBar/SideNavBar.jsx
@@ -0,0 +1,27 @@
+import { useAtomValue } from "jotai";
+import { Link } from "react-router-dom";
+import { userAtom } from "../../utilities/userContext";
+
+export default function SideNavBar() {
+  const user = useAtomValue(userAtom);
+
+  return (
+    <div className="uk-width-1-2@s uk-width-2-5@m">
+      <ul className="uk-nav uk-nav-default">
+        <li className="uk-nav-header">My Account</li>
+        <li>
+          <Link to={`/account/${user.id}/settings`}>Account Settings</Link>
+        </li>
+        <li>
+          <a href="#">Address</a>
+        </li>
+        <li>
+          <Link to={`/account/${user.id}/wishlist`}>Wishlist</Link>
+        </li>
+        <li>
+          <Link to={`/account/${user.id}/order-history`}>Order History</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}

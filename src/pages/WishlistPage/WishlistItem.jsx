478bd1eb64e23927a5af07e7ce9df773bd166be9
mindytyj <derpbritz@gmail.com>
2023-07-24 03:19:15 +0800

    Amend Wishlist Item and Page Styling

--- src/pages/WishlistPage/WishlistItem.jsx
+++ src/pages/WishlistPage/WishlistItem.jsx
@@ -13,7 +13,7 @@ export default function WishlistItem({ book, setWishlist, wishlist }) {
           alt=""
         />
       </td>
-      <td className="uk-text-nowrap">
+      <td className="uk-text-truncate">
         <Link to={`/books/${book.id}`} className="uk-button-link">
           {book.title}
         </Link>
--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -4,12 +4,12 @@ import WishlistTable from "./WishlistTable";
 export default function WishlistPage() {
   return (
     <div className="uk-container uk-padding">
-      <h1 className="uk-text-center">My Account</h1>
+      <h1 className="uk-text-center">My Wishlist</h1>
       <div data-uk-grid>
-        <div className="uk-width-1-3@m">
+        <div className="uk-width-1-3@s">
           <SideNavBar />
         </div>
-        <div className="uk-width-1-2@m">
+        <div className="uk-width-1-2@s">
           <WishlistTable />
         </div>
       </div>

2e879ecaeee9a2b793f024bcde33abab9de365e8
mindytyj <derpbritz@gmail.com>
2023-07-24 00:41:53 +0800

    Make Add and Minus Qty Button into Separate Components

---
+++ src/pages/CartPage/AddQtyButton.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import sendRequest from "../../utilities/send-request";
+import { useAtomValue } from "jotai";
+import { userAtom } from "../../utilities/userContext";
+
+export default function AddQtyButton({ bookId, totalQty, setTotalQty }) {
+  const user = useAtomValue(userAtom);
+
+  async function addQty() {
+    if (totalQty === 10) {
+      return;
+    }
+
+    try {
+      await sendRequest(`/api/carts/${user.id}/${bookId}`, "PUT", {
+        qty: parseInt(totalQty) + 1,
+      });
+      setTotalQty(totalQty + 1);
+    } catch {
+      console.log("Failed to add.");
+    }
+  }
+
+  return (
+    <button
+      uk-icon="plus"
+      ratio="0.5"
+      className="uk-icon-button uk-margin-small-right"
+      onClick={addQty}
+    ></button>
+  );
+}
---
+++ src/pages/CartPage/MinusQtyButton.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import sendRequest from "../../utilities/send-request";
+import { useAtomValue } from "jotai";
+import { userAtom } from "../../utilities/userContext";
+
+export default function MinusQtyButton({ bookId, totalQty, setTotalQty }) {
+  const user = useAtomValue(userAtom);
+
+  async function minusQty() {
+    if (totalQty === 1) {
+      return;
+    }
+
+    try {
+      await sendRequest(`/api/carts/${user.id}/${bookId}`, "PUT", {
+        qty: parseInt(totalQty) - 1,
+      });
+      setTotalQty(totalQty - 1);
+    } catch {
+      console.log("Failed to minus.");
+    }
+  }
+
+  return (
+    <button
+      uk-icon="minus"
+      ratio="0.5"
+      className="uk-icon-button uk-margin-small-left"
+      onClick={minusQty}
+    ></button>
+  );
+}

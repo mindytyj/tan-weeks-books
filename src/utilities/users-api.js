8860963fed76ee9d6c8234f83a0cc6921eede00a
mindytyj <derpbritz@gmail.com>
2023-07-24 15:42:37 +0800

    Update Users API in Utilities

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -9,6 +9,22 @@ export function login(credentials) {
   return sendRequest(`${BASE_URL}/login`, "POST", credentials);
 }
 
+export function updateFirstName(userId, firstName) {
+  return sendRequest(`${BASE_URL}/${userId}/firstName`, "PUT", firstName);
+}
+
+export function updateLastName(userId, lastName) {
+  return sendRequest(`${BASE_URL}/${userId}/lastName`, "PUT", lastName);
+}
+
+export function updateEmail(userId, email) {
+  return sendRequest(`${BASE_URL}/${userId}/email`, "PUT", email);
+}
+
+export function updatePassword(userId, password) {
+  return sendRequest(`${BASE_URL}/${userId}/password`, "PUT", password);
+}
+
 export function checkToken() {
   return sendRequest(`${BASE_URL}/check-token`);
 }

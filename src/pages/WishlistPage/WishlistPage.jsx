7b04ff35c020a0600105c0e2f18f1128bad953c7
mindytyj <derpbritz@gmail.com>
2023-07-25 00:35:31 +0800

    Update Wishlist Page Styling

--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -4,12 +4,12 @@ import WishlistTable from "./WishlistTable";
 export default function WishlistPage() {
   return (
     <div className="uk-container uk-padding">
-      <h1 className="uk-text-center">My Wishlist</h1>
       <div data-uk-grid>
-        <div className="uk-width-1-3@s">
+        <div className="uk-width-1-4@s">
           <SideNavBar />
         </div>
-        <div className="uk-width-1-2@s">
+        <div className="uk-card uk-card-default uk-card-body uk-width-3-4@s">
+          <h1 className="uk-text-center">My Wishlist</h1>
           <WishlistTable />
         </div>
       </div>

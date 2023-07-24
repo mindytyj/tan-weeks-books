f93b3c5ab57912fe4d4d160df15684e9b22d4cbb
mindytyj <derpbritz@gmail.com>
2023-07-24 15:28:18 +0800

    Update Account Page Styling

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -1,14 +1,17 @@
 import SideNavBar from "../../components/SideNavBar/SideNavBar";
+import AccountTable from "./AccountTable";
 
 export default function AccountPage() {
   return (
     <div className="uk-container uk-padding">
-      <h1 className="uk-text-center">My Account Settings</h1>
       <div data-uk-grid>
-        <div className="uk-width-1-3@s">
+        <div className="uk-width-1-4@s">
           <SideNavBar />
         </div>
-        <div className="uk-width-1-2@s"></div>
+        <div className="uk-card uk-card-default uk-card-body uk-width-3-4@s">
+          <h1 className="uk-text-center">My Account Settings</h1>
+          <AccountTable />
+        </div>
       </div>
     </div>
   );

403d6218b48d74516c494b72ae753d5b44a49527
mindytyj <derpbritz@gmail.com>
2023-07-25 19:32:30 +0800

    Fix Query Showing Wrong Order Data and Delete Cart Items upon Confirming Order

--- controllers/api/orders.js
+++ controllers/api/orders.js
@@ -5,7 +5,8 @@ async function retrieveOrders(req, res) {
 
   try {
     const orderHistory = await pool.query(
-      `SELECT books.id, books.title, books.image_url, order_details.qty, order_details.total FROM order_details JOIN books ON books.id = order_details.book_id JOIN orders ON orders.user_id = ${userId}`
+      "SELECT books.id, books.title, books.image_url, order_details.qty, order_details.total FROM order_details JOIN books ON books.id = order_details.book_id JOIN orders ON orders.id = order_details.order_id WHERE orders.user_id = ($1)",
+      [userId]
     );
     if (!orderHistory)
       throw new Error("There is no order history for the user.");
@@ -21,12 +22,20 @@ async function addToOrder(req, res) {
       "INSERT INTO orders (user_id) VALUES ($1) RETURNING orders.id",
       [req.body.userId]
     );
-    const order = await req.body.cart.map((cartItem) => {
+    await req.body.cart.map((cartItem) => {
       pool.query(
         "INSERT INTO order_details (book_id, qty, total, order_id) VALUES ($1, $2, $3, $4)",
-        [cartItem.id, cartItem.qty, cartItem.total, orderId.rows[0].id]
+        [
+          cartItem.id,
+          cartItem.qty,
+          cartItem.price * cartItem.qty,
+          orderId.rows[0].id,
+        ]
       );
     });
+    await pool.query("DELETE from carts WHERE carts.user_id = ($1)", [
+      req.body.userId,
+    ]);
     res.status(200).json("Order placed successfully.");
   } catch (error) {
     res.status(400).json(error.message);

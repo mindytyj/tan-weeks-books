71e6bdeb8e009413c9a2f02aca77ab8ebcc8751d
mindytyj <derpbritz@gmail.com>
2023-07-25 15:22:43 +0800

    Create Book Review Page

---
+++ src/pages/BookReviewPage/BookReviewPage.jsx
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import sendRequest from "../../utilities/send-request";
+import { useEffect, useState } from "react";
+import BookReviewForm from "./BookReviewForm";
+
+export default function BookReviewPage() {
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    async function getBookDetails() {
+      setLoading(true);
+      const retrieveBook = await sendRequest(`/api/books/${id}`, "GET");
+      setBook(retrieveBook);
+      setLoading(false);
+    }
+    getBookDetails();
+  }, []);
+
+  return (
+    <div className="uk-container uk-padding">
+      <h1 className="uk-text-center">My Review</h1>
+      <h3 className="uk-text-center">
+        Share your thoughts with fellow readers.
+      </h3>
+      <br />
+      <BookReviewForm book={book} />
+    </div>
+  );
+}

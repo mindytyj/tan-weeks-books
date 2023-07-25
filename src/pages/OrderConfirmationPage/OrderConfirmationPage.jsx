edca78c0edc5c4f6c1e73358306842114e65320a
mindytyj <derpbritz@gmail.com>
2023-07-25 12:43:17 +0800

    Create Order Confirmation Page

---
+++ src/pages/OrderConfirmationPage/OrderConfirmationPage.jsx
@@ -0,0 +1,34 @@
+import { useAtomValue } from "jotai";
+import { useNavigate, useParams } from "react-router-dom";
+import { userAtom } from "../../utilities/userContext";
+
+export default function OrderConfirmationPage() {
+  const user = useAtomValue(userAtom);
+  const { userId } = useParams();
+  const navigate = useNavigate();
+
+  if (!user || user.id !== userId) {
+    navigate("/");
+  }
+
+  function returnToMain() {
+    navigate("/");
+  }
+
+  return (
+    <div className="uk-container uk-padding">
+      <h1 className="uk-text-center">Your Order is Confirmed!</h1>
+      <div className="uk-container uk-margin-left uk-margin-right">
+        <div className="uk-width-3-4 uk-align-center">
+          <h3 className="uk-text-center">Thank you for shopping with us.</h3>
+          <button
+            className="uk-button uk-button-secondary uk-align-center"
+            onClick={returnToMain}
+          >
+            Return to Homepage
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}

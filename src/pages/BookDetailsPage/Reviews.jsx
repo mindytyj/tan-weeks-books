5eb9c51a4abd61291fc57138e2e31e94041ec028
mindytyj <derpbritz@gmail.com>
2023-07-25 17:14:41 +0800

    Retrieve and Map User Reviews in Reviews Component

--- src/pages/BookDetailsPage/Reviews.jsx
+++ src/pages/BookDetailsPage/Reviews.jsx
@@ -1,43 +1,36 @@
+import { useEffect, useState } from "react";
+import sendRequest from "../../utilities/send-request";
+import { useAtomValue } from "jotai";
+import { bookAtom } from "./bookContext";
+import ReviewCard from "./ReviewCard";
+
 export default function Reviews() {
+  const book = useAtomValue(bookAtom);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    async function getReviews() {
+      try {
+        const allReviews = await sendRequest(
+          `/api/books/${book.id}/reviews`,
+          "GET"
+        );
+        setReviews(allReviews);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    getReviews();
+  }, []);
+
   return (
-    <article class="uk-comment" role="comment">
-      <header class="uk-comment-header">
-        <div class="uk-grid-medium uk-flex-middle" uk-grid>
-          <div class="uk-width-auto">
-            <img
-              class="uk-comment-avatar"
-              src="images/avatar.jpg"
-              width="80"
-              height="80"
-              alt=""
-            />
-          </div>
-          <div class="uk-width-expand">
-            <h4 class="uk-comment-title uk-margin-remove">
-              <a class="uk-link-reset" href="#">
-                Author
-              </a>
-            </h4>
-            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
-              <li>
-                <a href="#">12 days ago</a>
-              </li>
-              <li>
-                <a href="#">Reply</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
-      <div class="uk-comment-body">
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet.
-        </p>
+    <div className="uk-section uk-section-muted uk-preserve-color">
+      <div className="uk-container uk-margin-left uk-margin-right">
+        <h2 className="uk-text-center">Customer Reviews</h2>
+        {reviews.map((review) => {
+          return <ReviewCard review={review} key={review.id} />;
+        })}
       </div>
-    </article>
+    </div>
   );
 }

4c95968d3873dd5e442cf7fa83a063fc78092507
mindytyj <derpbritz@gmail.com>
2023-07-26 12:32:03 +0800

    Add Book Button Conditional Render if User is Admin

--- src/pages/BooksPage/BooksPage.jsx
+++ src/pages/BooksPage/BooksPage.jsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import sendRequest from "../../utilities/send-request";
 import BookCard from "./BookCard";
+import { useAtomValue } from "jotai";
+import { userAtom } from "../../utilities/userContext";
+import { adminAtom } from "../../utilities/adminContext";
+import AddBookButton from "../AddBookPage/AddBookButton";
 
 export default function BooksPage() {
   const [books, setBooks] = useState([]);
+  const user = useAtomValue(userAtom);
+  const isAdmin = useAtomValue(adminAtom);
 
   useEffect(() => {
     async function getAllBooks() {
@@ -21,6 +27,7 @@ export default function BooksPage() {
     <div className="uk-container uk-padding">
       <div className="uk-section uk-section- uk-preserve-color">
         <div className="uk-container uk-margin-left uk-margin-right">
+          {user && isAdmin ? <AddBookButton /> : <></>}
           <div
             className="uk-grid-column-medium uk-child-width-1-4@s uk-grid-match uk-text-center"
             data-uk-grid

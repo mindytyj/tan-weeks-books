b0e99706f8ec178e704ff355ed116b055a9886d4
mindytyj <derpbritz@gmail.com>
2023-07-27 02:48:24 +0800

    Amend AuthPage to Return Outlet

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,20 +1,11 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
-import SignUpPage from "../SignUpPage/SignUpPage";
-import LoginPage from "../LoginPage/LoginPage";
+export default function AuthPage({ user }) {
+  const navigate = useNavigate();
 
-export default function AuthPage() {
-  const [alreadyUser, setAlreadyUser] = useState(true);
-  const location = useLocation();
+  if (!user) {
+    navigate("/login");
+  }
 
-  useEffect(() => {
-    if (location.pathname === "/signup") {
-      setAlreadyUser(false);
-    } else {
-      setAlreadyUser(true);
-    }
-  }, [location]);
-
-  return <main>{alreadyUser ? <LoginPage /> : <SignUpPage />}</main>;
+  return <Outlet />;
 }

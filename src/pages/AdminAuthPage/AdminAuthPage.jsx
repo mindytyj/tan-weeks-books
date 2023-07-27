5b7ac2db7331076e79d978126b5c20042954900b
mindytyj <derpbritz@gmail.com>
2023-07-27 11:54:45 +0800

    Include Use Effect for Admin Auth Page

--- src/pages/AdminAuthPage/AdminAuthPage.jsx
+++ src/pages/AdminAuthPage/AdminAuthPage.jsx
@@ -1,14 +1,17 @@
 import { useAtomValue } from "jotai";
 import { Outlet, useNavigate } from "react-router-dom";
 import { adminAtom } from "../../utilities/adminContext";
+import { useEffect } from "react";
 
 export default function AdminAuthPage({ user }) {
   const isAdmin = useAtomValue(adminAtom);
   const navigate = useNavigate();
 
-  if (!user || !isAdmin) {
-    navigate("/");
-  }
+  useEffect(() => {
+    if (!user || !isAdmin) {
+      navigate("/");
+    }
+  }, [isAdmin]);
 
   return <Outlet />;
 }

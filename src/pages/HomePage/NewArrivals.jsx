9ac7a253b08214932eaef74a72d83cd23d07e7dc
mindytyj <derpbritz@gmail.com>
2023-07-27 10:37:27 +0800

    Amend and Set-Up New Arrivals Component

--- src/pages/HomePage/NewArrivals.jsx
+++ src/pages/HomePage/NewArrivals.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import sendRequest from "../../utilities/send-request";
+import { Link } from "react-router-dom";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
+
 export default function NewArrivals() {
+  const [loading, setLoading] = useState(true);
+  const [newArrivals, setNewArrivals] = useState([]);
+
+  useEffect(() => {
+    async function getNewArrivals() {
+      try {
+        setLoading(true);
+        const books = await sendRequest("/api/books/newArrivals", "GET");
+        setNewArrivals(books);
+        setLoading(false);
+      } catch {
+        console.error("Failed to retrieve new arrivals.");
+      }
+    }
+    getNewArrivals();
+  }, []);
+
+  if (loading === true) {
+    return <LoadingScreen />;
+  }
+
   return (
     <div>
       <div className="uk-h3 uk-text-center">New Arrivals</div>
@@ -8,100 +34,34 @@ export default function NewArrivals() {
         data-uk-slider
       >
         <ul className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-grid">
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>1</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>2</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>3</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>4</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>5</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>6</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>7</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>8</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>9</h1>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="uk-panel">
-              <img src="" width="400" height="600" alt="" />
-              <div className="uk-position-center uk-panel">
-                <h1>10</h1>
-              </div>
-            </div>
-          </li>
+          {newArrivals.map((newArrival) => {
+            return (
+              <li key={newArrival?.id}>
+                <div className="uk-panel">
+                  <Link to={`/books/${newArrival?.id}`}>
+                    <img
+                      src={newArrival?.image_url}
+                      width="400"
+                      height="600"
+                      alt={newArrival?.title}
+                    />
+                  </Link>
+                </div>
+              </li>
+            );
+          })}
         </ul>
 
-        <a
-          className="uk-position-center-left uk-position-small uk-hidden-hover"
-          href="#"
+        <Link
+          className="uk-position-center-left uk-position-small uk-hidden-hover uk-slidenav-large"
           data-uk-slidenav-previous
           data-uk-slider-item="previous"
-        ></a>
-        <a
-          className="uk-position-center-right uk-position-small uk-hidden-hover"
-          href="#"
+        ></Link>
+        <Link
+          className="uk-position-center-right uk-position-small uk-hidden-hover uk-slidenav-large"
           data-uk-slidenav-next
           data-uk-slider-item="next"
-        ></a>
+        ></Link>
       </div>
     </div>
   );

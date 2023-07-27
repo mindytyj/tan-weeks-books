10c5d207d1616980cc47c64147996ea0e21f445a
mindytyj <derpbritz@gmail.com>
2023-07-27 11:46:52 +0800

    Amend and Set-Up Promotions Component

--- src/pages/HomePage/Promotions.jsx
+++ src/pages/HomePage/Promotions.jsx
@@ -1,37 +1,35 @@
-export default function Promotions() {
+import { Link } from "react-router-dom";
+
+export default function Promotions({ promotions }) {
   return (
     <div className="uk-container uk-align-center">
       <div className="uk-h3 uk-text-center">T-WB Promotions</div>
 
       <div
         className="uk-position-relative uk-visible-toggle uk-light"
-        tabindex="-1"
-        uk-slideshow="animation: fade"
+        data-tabindex="-1"
+        data-uk-slideshow="animation: fade"
       >
         <ul className="uk-slideshow-items">
-          <li>
-            <img src="" alt="" uk-cover />
-          </li>
-          <li>
-            <img src="" alt="" uk-cover />
-          </li>
-          <li>
-            <img src="" alt="" uk-cover />
-          </li>
+          {promotions.map((promotion) => {
+            return (
+              <li key={promotion.id}>
+                <img src={promotion.promotion_url} alt="" data-uk-cover />
+              </li>
+            );
+          })}
         </ul>
 
-        <a
-          className="uk-position-center-left uk-position-small uk-hidden-hover"
-          href="#"
-          uk-slidenav-previous
-          uk-slideshow-item="previous"
-        ></a>
-        <a
-          className="uk-position-center-right uk-position-small uk-hidden-hover"
-          href="#"
-          uk-slidenav-next
-          uk-slideshow-item="next"
-        ></a>
+        <Link
+          className="uk-position-center-left uk-position-small uk-hidden-hover uk-slidenav-large"
+          data-uk-slidenav-previous
+          data-uk-slideshow-item="previous"
+        ></Link>
+        <Link
+          className="uk-position-center-right uk-position-small uk-hidden-hover uk-slidenav-large"
+          data-uk-slidenav-next
+          data-uk-slideshow-item="next"
+        ></Link>
       </div>
     </div>
   );

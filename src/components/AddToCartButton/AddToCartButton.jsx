21b341229493d18604d5c7d64a155726587a673c
mindytyj <derpbritz@gmail.com>
2023-07-27 19:01:20 +0800

    Add Navigation after Add to Cart Button is Clicked

--- src/components/AddToCartButton/AddToCartButton.jsx
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -1,14 +1,16 @@
 import { useAtomValue } from "jotai";
 import { userAtom } from "../../utilities/userContext";
 import sendRequest from "../../utilities/send-request";
+import { useNavigate } from "react-router-dom";
 
 export default function AddToCartButton({ book }) {
   const disable = parseInt(book.qty) === 0;
   const user = useAtomValue(userAtom);
+  const navigate = useNavigate();
 
   async function addToCart() {
     if (user === null) {
-      return;
+      return navigate("/login");
     }
 
     try {
@@ -17,8 +19,10 @@ export default function AddToCartButton({ book }) {
         qty: 1,
         userId: parseInt(user.id),
       });
-    } catch (error) {
-      console.log(error.message);
+      navigate("/cart");
+    } catch {
+      console.log("Book is already in cart.");
+      navigate("/cart");
     }
   }
 
